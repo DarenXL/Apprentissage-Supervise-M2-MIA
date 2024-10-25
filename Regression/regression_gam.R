28ab7473b20b722ec97bca9f15f63611f20de1ff
Alexandre <alexou.loret@gmail.com>
2024-10-25 14:13:57 +0200

    update gam

--- Regression/regression_gam.R
+++ Regression/regression_gam.R
@@ -11,7 +11,7 @@ test <- read_parquet('data/modified/test_modified.parquet')[c(-1,-2)]
 ###################################################################
 #### GAM + RF
 
-RFGAM <- function(train0, test1, equation, cov){
+RFGAM <- function(train0, test1, equation, cov, custom_gterm=NULL){
 
   # GAM
   gam.res <- gam(equation, data = train0)                     # GAM sur train
@@ -30,12 +30,20 @@ RFGAM <- function(train0, test1, equation, cov){
   train1_rf <- data.frame(test1, terms1)                      # Estimation des résidus de test
 
   # Equation pour les résidus
-  cov <- paste0(c(cov, colnames(terms0)),collapse=' + ')
+  if (is.null(custom_gterm)) {
+    cov <- paste0(c(cov, colnames(terms0)),collapse=' + ')
+  }else{
+    cov <- paste0(c(cov, colnames(terms0)[custom_gterm]),collapse=' + ')
+  }
   equation_rf <-  paste0("res", " ~ ", cov)
 
   # RF sur les résidus
-  rf_gam.res <- ranger::ranger(equation_rf, data = train0_rf, importance =  'permutation')
+  rf_gam.res <- ranger::ranger(equation_rf,
+                               data = train0_rf,
+                               importance =  'permutation',
+                               num.trees = 500) # , max.depth = 35, mtry=5)
   rf_gam.forecast <- predict(rf_gam.res, data = train1_rf)$predictions + gam.forecast1
+  print(rf_gam.res$variable.importance)
 
   return(rf_gam.forecast)
 }
@@ -48,41 +56,57 @@ R2 <- function(y_actual,y_predict){
 }
 
 # Cross Validation
-CV <- function(data, equation, equation_rf, iter=5){
+CV <- function(data, equation, equation_rf, gterm, K=5){
   cv_R2 <- 0
-  for(i in 1:iter){
-    sample <- sample(c(TRUE, FALSE), nrow(data), replace=TRUE, prob=c(0.7,0.3))
-    train0  <- data[sample, ]
-    test1   <- data[!sample, ]
 
-    # pred <- RFGAM(train0, test1, equation, equation_rf)
+  ind <- sample(1:K,nrow(data),replace=T)
+  for(i in 1:K){
 
-    gam.res <- gam(equation, data = train0)
-    pred <- pmax(predict(gam.res, newdata=test1),0)
+    print(paste0("--> fold ", i))
 
-    # print(summary(gam.res))
+    train0  <- data[ind != i, ]
+    test1   <- data[ind == i, ]
+
+    pred <- RFGAM(train0, test1, equation, equation_rf, gterm)
+
+    # gam.res <- gam(equation, data = train0)
+    # pred <- pmax(predict(gam.res, newdata=test1),0)
 
     cv_R2 <- cv_R2 + R2(test1$tip_amount, pred)
   }
-  return(cv_R2/iter)
+  return(cv_R2/K)
 }
 
-equation <- tip_amount ~ passenger_count + trip_distance + fare_amount + extra + mta_tax + tolls_amount + improvement_surcharge +
-  congestion_surcharge + Airport_fee + PU_location_lat + PU_location_lon + DO_location_lat + DO_location_lon + VendorID_2 +
-  RatecodeID_2.0 + RatecodeID_3.0 + RatecodeID_4.0 + RatecodeID_5.0 + RatecodeID_6.0
-
-equation <- tip_amount ~ passenger_count + s(trip_distance) + fare_amount + extra + tolls_amount +
+equation <- tip_amount ~ passenger_count + s(trip_distance, bs='cr') + s(fare_amount, bs='cr') + s(extra) + s(tolls_amount, bs='cr') +
   congestion_surcharge + Airport_fee + te(PU_location_lat + PU_location_lon) + te(DO_location_lat + DO_location_lon) + VendorID_2 +
-  RatecodeID_2.0 + RatecodeID_3.0 + RatecodeID_4.0 + RatecodeID_5.0 + RatecodeID_6.0
+  RatecodeID_2.0 + RatecodeID_3.0 + RatecodeID_4.0 + RatecodeID_5.0 + RatecodeID_6.0 + store_and_fwd_flag_Y + payment_type_2 +
+  payment_type_3 + payment_type_4 + s(duration, bs='cr') + s(log_duration, bs='cr') + s(hour, bs='tp') + week_end + day
 
-cov <- c("trip_distance","extra")
+# best : seulement les variables les plus significatives pour le GAM
+equation <- tip_amount ~ s(trip_distance, bs='cr') + s(fare_amount, bs='cr') + s(extra) + s(tolls_amount, bs='cr') +
+  congestion_surcharge + Airport_fee + te(PU_location_lat + PU_location_lon) + te(DO_location_lat + DO_location_lon) + VendorID_2 +
+  RatecodeID_2.0 + RatecodeID_3.0 + RatecodeID_4.0 + RatecodeID_5.0 + RatecodeID_6.0 + payment_type_2 +
+  payment_type_3 + payment_type_4 + s(duration, bs='cr') + s(log_duration, bs='cr') + s(hour, bs='tp')
 
-CV(train, equation, cov, iter=3)
+# best : la majorité des variables sont utilisées pour prédire les résidus
+custom_gterm <- NULL
+cov <- c("trip_distance","fare_amount","tolls_amount","log_duration","hour","extra", "week_end", "day", "passenger_count","PU_location_lat", "PU_location_lon","DO_location_lat", "DO_location_lon")
+
+# Les termes avec le plus d'importance dans la random forest
+# custom_gterm <- c(3,5,11,16,17,22,23)
+custom_gterm <- c(9,12,13,18,19)
+cov <- c("trip_distance","fare_amount","log_duration")
+
+CV(train, equation, cov, custom_gterm, K=5)
+
+# overfitting ?
+pred <- RFGAM(train, train, equation, cov,custom_gterm)
+R2(train$tip_amount, pred)
 
 ###########################################################
 #### Submit
 
-res <- RFGAM(train, test, equation, cov)
+res <- RFGAM(train, test, equation, cov,custom_gterm)
 
 data.res <- data.frame(row_ID = 1:length(res), tip_amount = res)
 write_parquet(data.res,"predictions/GAMRF.parquet")
