355ecbe3237b10368c5b4707765620bb7f57d8c7
Alexandre <alexou.loret@gmail.com>
2024-11-12 14:56:53 +0100

    petites modifs

--- Regression/regression_gam.R
+++ Regression/regression_gam.R
@@ -7,11 +7,13 @@ library(ranger)
 library(tidytable)
 library(lubridate)
 
-# train <- read_parquet('data/modified/train_modified.parquet')[c(-1,-2)]
-# test <- read_parquet('data/modified/test_modified.parquet')[c(-1,-2)]
+# Données produites par location.ipynb
+train <- read_parquet('data/train2.parquet')
+test <- read_parquet('data/test2.parquet')
 
-train <- read_parquet('data/train.parquet')
-test <- read_parquet('data/test.parquet')
+# Données originales
+# train <- read_parquet('data/train.parquet')
+# test <- read_parquet('data/test.parquet')
 
 train[train$RatecodeID >= 6, 'RatecodeID'] <- 6.0
 test[test$RatecodeID >= 6, 'RatecodeID'] <- 6.0
@@ -22,14 +24,23 @@ train <- train[,-c('VendorID', 'RatecodeID', 'store_and_fwd_flag', 'payment_type
 test <- get_dummies(test, cols=c('VendorID', 'RatecodeID', 'store_and_fwd_flag', 'payment_type'))
 test <- test[,-c('VendorID', 'RatecodeID', 'store_and_fwd_flag', 'payment_type')]
 
-# train$tpep_pickup_datetime <- as.POSIXct(train$tpep_pickup_datetime,tz=Sys.timezone())
-# test$tpep_pickup_datetime <- as.POSIXct(test$tpep_pickup_datetime,tz=Sys.timezone())
-
 train$duration <- as.numeric(train$tpep_dropoff_datetime) - as.numeric(train$tpep_pickup_datetime)
 test$duration <- as.numeric(test$tpep_dropoff_datetime) - as.numeric(test$tpep_pickup_datetime)
 
 train$log_duration <- log(train$duration + 1)
 test$log_duration <- log(abs(test$duration) + 1)
+
+train$long_trip <- 1.0* (train$duration > 70000)
+test$long_trip <- 1.0 * (test$duration > 70000)
+
+train$euclidian_distance <- sqrt( (train$DO_location_lon - train$PU_location_lon)^2 + (train$DO_location_lat - train$PU_location_lat)^2 )
+test$euclidian_distance <- sqrt( (test$DO_location_lon - test$PU_location_lon)^2 + (test$DO_location_lat - test$PU_location_lat)^2 )
+
+train$speed <- train$trip_distance/(train$duration + 1e-3)
+test$speed <- test$trip_distance/(test$duration + 1e-3)
+
+train$log_speed <- log(train$speed)
+test$log_speed <- log(abs(test$speed))
 
 train$hour <- hour(train$tpep_dropoff_datetime) + minute(train$tpep_dropoff_datetime) / 60
 test$hour <- hour(test$tpep_dropoff_datetime) + minute(test$tpep_dropoff_datetime) / 60
@@ -40,8 +51,17 @@ test$week_end <- (wday(test$tpep_dropoff_datetime) >= 6)
 train$day <- mday(train$tpep_dropoff_datetime)
 test$day <- mday(test$tpep_dropoff_datetime)
 
+train$ferie <- 1.0 * ( (train$day == 1) | (train$day == 15) )
+test$ferie <- 1.0 * ( (test$day == 1) | (test$day == 15) )
+
 train <- train[,-c("tpep_pickup_datetime", "tpep_dropoff_datetime")]
 test <- test[,-c("tpep_pickup_datetime", "tpep_dropoff_datetime")]
+
+# /!\ Cas d'utilisation de train2 et test2
+train <- get_dummies(train, cols=c('PU_location','DO_location'))
+train <- train[,-c('PU_location', 'DO_location', 'PU_location_outside', 'DO_location_outside')]
+test <- get_dummies(test, cols=c('PU_location','DO_location'))
+test <- test[,-c('PU_location', 'DO_location', 'PU_location_outside', 'DO_location_outside')]
 
 ###################################################################
 #### GAM + RF
@@ -104,29 +124,49 @@ CV <- function(data, equation, equation_rf, gterm, K=5){
 
     pred <- RFGAM(train0, test1, equation, equation_rf, gterm)
 
-    #gam.res <- gam(equation, data = train0)
-    #pred <- pmax(predict(gam.res, newdata=test1),0)
+    # gam.res <- gam(equation, data = train0)
+    # pred <- pmax(predict(gam.res, newdata=test1),0)
 
     cv_R2 <- cv_R2 + R2(test1$tip_amount, pred)
   }
   return(cv_R2/K)
 }
 
+# la majorité des variables
+equation <- tip_amount ~ passenger_count + s(trip_distance) + s(fare_amount) + s(extra) + mta_tax +
+  s(tolls_amount) + improvement_surcharge + congestion_surcharge + Airport_fee +
+  te(PU_location_lat, PU_location_lon) + te(DO_location_lat, DO_location_lon) +
+  VendorID_2 + RatecodeID_2 + RatecodeID_3 + RatecodeID_4 + RatecodeID_5 + RatecodeID_6 + store_and_fwd_flag_Y +
+  payment_type_2 + payment_type_3 + payment_type_4 + s(duration) + s(log_duration) + long_trip + s(euclidian_distance) +
+  s(speed) + s(log_speed) + s(hour) + week_end + day +
+  PU_location_airport + PU_location_bronx + PU_location_brooklyn + PU_location_manhattan + PU_location_queens + PU_location_staten_island +
+  DO_location_airport + DO_location_bronx + DO_location_brooklyn + DO_location_manhattan + DO_location_queens + DO_location_staten_island
+
+gam.res <- gam(equation, data = train)
+summary(gam.res)
+
 # best : seulement les variables les plus significatives pour le GAM
-equation <- tip_amount ~ s(trip_distance, bs='cr') + s(fare_amount, bs='cr') + s(extra) + s(tolls_amount, bs='cr') +
-  congestion_surcharge + Airport_fee + te(PU_location_lat + PU_location_lon) + te(DO_location_lat + DO_location_lon) + VendorID_2 +
-  RatecodeID_2 + RatecodeID_3 + RatecodeID_4 + RatecodeID_5 + RatecodeID_6 + payment_type_2 +
-  payment_type_3 + payment_type_4 + s(duration, bs='cr') + s(log_duration, bs='cr') + s(hour, bs='tp')
+equation <- tip_amount ~ s(trip_distance, bs='cr') + s(fare_amount, bs='cr') + s(extra) + mta_tax + s(tolls_amount, bs='cr') + congestion_surcharge +
+  te(PU_location_lat, PU_location_lon) + te(DO_location_lat, DO_location_lon) +
+  VendorID_2 + RatecodeID_2 + RatecodeID_4 + RatecodeID_6 +
+  payment_type_2 + payment_type_3 + payment_type_4 + s(log_duration, bs='cr') + s(euclidian_distance) + s(log_speed, bs='cr') + s(hour, bs='tp') +
+  PU_location_airport + PU_location_bronx + PU_location_brooklyn + PU_location_manhattan + PU_location_queens +
+  DO_location_airport + DO_location_bronx + DO_location_brooklyn + DO_location_manhattan + DO_location_queens + DO_location_staten_island
 
 # best : la majorité des variables sont utilisées pour prédire les résidus
 custom_gterm <- NULL
-cov <- c("trip_distance","fare_amount","tolls_amount","log_duration","hour","extra", "week_end", "day", "passenger_count","PU_location_lat", "PU_location_lon","DO_location_lat", "DO_location_lon",
-         "RatecodeID_2","RatecodeID_3","RatecodeID_4","RatecodeID_5","RatecodeID_6", "VendorID_2", "payment_type_2", "payment_type_3", "payment_type_4")
-
+cov <- c("passenger_count", "trip_distance", "fare_amount", "extra", "mta_tax",
+  "tolls_amount", "improvement_surcharge", "congestion_surcharge", "Airport_fee",
+  "PU_location_lat", "PU_location_lon", "DO_location_lat", "DO_location_lon",
+  "VendorID_2", "RatecodeID_2", "RatecodeID_3", "RatecodeID_4", "RatecodeID_5", "RatecodeID_6", "store_and_fwd_flag_Y",
+  "payment_type_2", "payment_type_3", "payment_type_4", "duration", "log_duration", "long_trip", "euclidian_distance",
+  "speed", "log_speed", "hour", "week_end", "day",
+  "PU_location_airport", "PU_location_bronx", "PU_location_brooklyn", "PU_location_manhattan", "PU_location_queens", "PU_location_staten_island",
+  "DO_location_airport", "DO_location_bronx", "DO_location_brooklyn", "DO_location_manhattan", "DO_location_queens", "DO_location_staten_island")
 
 # Les termes avec le plus d'importance dans la random forest
-# custom_gterm <- c(2, 9, 12, 13, 18, 19)
-# cov <- c("trip_distance","fare_amount","log_duration")
+# custom_gterm <- c(21, 22, 27, 28, 29)
+# cov <- c("trip_distance","fare_amount","payment_type_2","duration","log_duration","euclidian_distance","speed","log_speed")
 
 CV(train, equation, cov, custom_gterm, K=5)
 
